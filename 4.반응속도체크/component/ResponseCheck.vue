1f8bf4b4135c9c7abea37ad10332bc1da6708d14
sunmyounglee <roeowo123@naver.com>
2021-08-06 19:14:31 +0900

    webpack --watch 추가

--- 4.반응속도체크/component/ResponseCheck.vue
+++ 4.반응속도체크/component/ResponseCheck.vue
@@ -3,6 +3,7 @@
         <div id='screen'>{{}}</div>
         <div>
             <div> 평균시간 : {{}}</div>
+            <button @click="onReset">리셋</button>
         </div>
     </div>
 </template>
@@ -12,9 +13,15 @@
 
         data() {
             return {
+                result:[],
+                state:'waiting',
+                meesage:'클릭해서 시작하세요.',
             }
         },
         methods: {
+            onReset() {
+
+            }
         }
     }
 </script>
