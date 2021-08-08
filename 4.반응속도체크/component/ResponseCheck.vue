0651b7cfbb771ca66d0faf5fd6fc012643263854
sunmyounglee <roeowo123@naver.com>
2021-08-08 16:52:08 +0900

    반응속도체크 완성

--- 4.반응속도체크/component/ResponseCheck.vue
+++ 4.반응속도체크/component/ResponseCheck.vue
@@ -2,35 +2,52 @@
     <div>
         <!-- 클래스에 데이터를 넣고 싶으면 바인드를 넣어준다. -->
         <!-- v-bind 의 축약형 => : -->
-        <div id='screen' :class="state" @click="onClickScreen">{{meesage}}</div>
+        <div id='screen' :class="state" @click="onClickScreen">{{message}}</div>
         <div>
-            <div> 평균시간 : {{}}</div>
+            <!-- 평균구하는 코드-->
+            <!-- 배열을 다 더하는 함수 -->
+            <div> 평균시간 : {{result.reduce((a,c) => a+c,0)/result.length|| 0}} ms</div>
             <button @click="onReset">리셋</button>
         </div>
     </div>
 </template>
 
 <script>
+    let startTime = 0;
+    let endTime = 0;
+    let timeout = null;
     export default {
 
         data() {
             return {
+                //화면에 관련있는애들만 넣는다.
                 result:[],
                 state:'waiting',
-                meesage:'클릭해서 시작하세요.',
+                message:'클릭해서 시작하세요.',
             }
         },
         methods: {
             onReset() {
-
+                this.result=[];
             },
             onClickScreen() {
                 if ( this.state === 'waiting') {
-                    this.state = 'ready'
+                    this.state = 'ready';
+                    this.message = '초록색이 되면 클릭하세요';
+                    timeout = setTimeout(() => {
+                        this.state = 'now'
+                        this.message = '지금 클릭!';
+                        startTime = new Date();
+                    }, Math.floor(Math.random() * 1000)+ 2000); //2~3초
                 } else if ( this.state === 'ready') {
-                    this.state = 'now'
-                } else if ( this.state === 'now') {
+                    clearTimeout(timeout);
                     this.state = 'waiting'
+                    this.message = '너무 성급하시군요! 초록색이 된후 클릭하세요!';
+                } else if ( this.state === 'now') {
+                    endTime = new Date();
+                    this.state = 'waiting'
+                    this.message = '클릭해서 시작하세요.';
+                    this.result.push(endTime - startTime);
                     };
                 },
         }
