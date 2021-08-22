4e501cb4dd6d6f62fc58a6926e1ccf187158a364
jsleemaster <roeowo123@naver.com>
2021-08-22 22:25:19 +0900

    slot 사용

--- 7.틱택토vuex/component/TdComponent.vue
+++ 7.틱택토vuex/component/TdComponent.vue
@@ -3,7 +3,6 @@
 </template>
 <script>
     import { mapState} from 'vuex'
-    import {CLICK_CELL,SET_WINNER , RESET_GAME, CHANGE_TURN, NO_WINNER} from '../store.js';
 export default {
     props: {
         rowIndex :Number,
@@ -28,48 +27,7 @@ export default {
         // }
     },
     methods:{
-        onClickTd(){
-            if (this.cellData) return ;
-
-            this.$store.commit(CLICK_CELL, {row: this.rowIndex, cell: this.cellIndex});
-
-            let win = false;
-            if (this.tableData[this.rowIndex][0] === this.turn && this.tableData[this.rowIndex][1] === this.turn && this.tableData[this.rowIndex][2] === this.turn ) {
-                win = true;
-            }
-            if (this.tableData[0][this.cellIndex] === this.turn && this.tableData[1][this.cellIndex] === this.turn && this.tableData[2][this.cellIndex] === this.turn ) {
-                win = true;
-            }
-            if (this.tableData[0][0] === this.turn && this.tableData[1][1] === this.turn && this.tableData[2][2] === this.turn ) {
-                win = true;
-            }
-            if (this.tableData[0][2] === this.turn && this.tableData[1][1] === this.turn && this.tableData[2][0] === this.turn ) {
-                win = true;
-            }
-            if (win) {
-                //이긴경우 3줄달성
-                this.$store.commit(SET_WINNER, this.turn)
-                this.$store.commit(RESET_GAME)
-            } else {
-                //무승부
-                let all = true; // all이 true면 무승부라는 뜻
-                this.tableData.forEach((row)=> {
-                    row.forEach((cell)=> {
-                        if(!cell) {
-                            all = false;
-                        }
-                    })
-                })
-                if(all) {
-                    //무승부
-                    this.$store.commit(NO_WINNER)
-                    this.$store.commit(RESET_GAME)
-                } else {
-                    this.$store.commit(CHANGE_TURN)
-                }
-                
-            }
-            }
+       
     }
 }
 </script>
