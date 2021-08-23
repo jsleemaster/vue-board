05b432bca466ab5d7e0144d8ce90fcd80ee7501e
jsleemaster <roeowo123@naver.com>
2021-08-23 22:39:36 +0900

    지뢰찾기 vuex state 적용

---
+++ 8.지뢰찾기/MineSweeper.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <mine-form/>
+        <div>{{timer}}</div>
+        <table-component></table-component>
+        <div>{{result}}</div>
+    </div>
+</template>
+<script>
+    import { mapState} from 'vuex'
+    // import store from '../store'
+    import store, {} from '../store.js';
+// import TableComponent from '../../7.틱택토/component/TableComponent.vue';
+
+    export default {
+        store,
+        components: {
+        },
+        computed:{ 
+            ...mapState(['timer','result'])
+        },
+        methods :{
+        },
+    }
+</script>
+<style>
+</style>
