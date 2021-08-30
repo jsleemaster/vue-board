e12698d961e6883276f0c807be74e18040d3e626
jsleemaster <roeowo123@naver.com>
2021-08-30 21:58:58 +0900

    라우터 연결

---
+++ vue-router/main.js
@@ -0,0 +1,5 @@
+import Vue from 'vue';
+import MineSweeper from './MineSweeper';
+
+// Vue.config.productionTip = false;
+new Vue(MineSweeper).$mount('#root');
---
+++ vue-router/router.js
@@ -0,0 +1,14 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import NumberBaseball from '../3.숫자야구/NumberBaseball'
+import ResponseCheck from '../4.반응속도체크/ResponseCheck'
+import RockScissorsPaper from '../5.가위바위보/RockScissorsPaper'
+Vue.use(VueRouter)
+
+export default new VueRouter({
+    routes: [
+        { path: '/number-baseball', component: NumberBaseball },
+        { path: '/response-check', component: ResponseCheck },
+        { path: '/rock-scissors-paper', component: RockScissorsPaper },
+    ]
+})
